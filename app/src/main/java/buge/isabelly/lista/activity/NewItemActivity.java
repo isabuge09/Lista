6b11fc8523a9f3010ef633e10df4b4e79cbce1e6
Isabelly Eduarda Buge <isabellyeduardabuge@gmail.com>
2024-04-23 09:30:46 -0300

    continuacao parte 3 atividade

--- app/src/main/java/buge/isabelly/lista/activity/NewItemActivity.java
+++ app/src/main/java/buge/isabelly/lista/activity/NewItemActivity.java
@@ -1,12 +1,16 @@
 package buge.isabelly.lista.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import buge.isabelly.lista.R;
 
@@ -20,12 +24,26 @@ public class NewItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
 
-        ImageButton imgCI = findViewById(R.id.imbCI);
-        imgCI.setOnClickListener(new View.OnClickListener() {
+        ImageButton imgCI = findViewById(R.id.imbCI);//obtemos o botao pelo seu id
+        imgCI.setOnClickListener(new View.OnClickListener() {//obtem o click do botao
             @Override
-            public void onClick(View v) {
-                Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            public void onClick(View v) {//metodo executado quando apos o click do botao
+                Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);//cria de um Intent implicito que possui acao de abrir documento
+                photoPickerIntent.setType("image/*");//especificamos que estamos interessados em documentos que sao imagens
+                startActivityForResult(photoPickerIntent, PHOTO_PICKER_REQUEST); //metodo que executa o Intent
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PHOTO_PICKER_REQUEST){
+            if(resultCode == Activity.RESULT_OK) {
+                photoSelected = data.getData();
+                ImageView imvfotoPreview = findViewById(R.id.imvPhotoPreview);
+                imvfotoPreview.setImageURI(photoSelected);
+            }
+        }
     }
 }
